7f31c7fc4f8eea47b5f655f8cb82f1cf23c3134a
Pablo Araujo <pabloarro2005@gmail.com>
2026-08-13 19:49:44 +0200

    mappers adjustment

--- lib/api/mappers.ts
+++ lib/api/mappers.ts
@@ -4,7 +4,7 @@ import { Dish } from "@/types/planner";
 export function mapPlatoToDish(plato: PlatoAPI): Dish {
   return {
     id: plato.id.toString(),
-    title: plato.nombre,
-    labels: plato.etiquetas.map(e => e.nombre),
+    title: plato.name,
+    labels: plato.tags.map(e => e.name),
   };
 }
